77b23ed36cab369390e78052d5fbdf6318d887ad
yunzheng <42thcoder@gmail.com>
2016-11-07 17:37:14 +0800

    改良下单元测试

--- src/test/java/com/hackerzhang/dust/LexerTest.java
+++ src/test/java/com/hackerzhang/dust/LexerTest.java
@@ -2,12 +2,31 @@ package com.hackerzhang.dust;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.Assert.assertEquals;
 
 public class LexerTest {
     @Test
     public void testLexer() throws ParseException {
         Lexer lexer = new Lexer(new CodeDialog());
+
+        System.out.println("Ready for the tokens!");
+
+        for(Token token; (token = lexer.read()) != Token.EOF; )
+            System.out.println("=> " + token.getText());
+
+        System.out.println("Here ends the show");
+
+        assertEquals("hello", "hello");
+    }
+
+    @Test
+    public void testGistLexer() throws ParseException, FileNotFoundException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        FileReader fileReader = new FileReader(classLoader.getResource("fixtures/gist1.txt").getFile());
+        Lexer lexer = new Lexer(fileReader);
 
         System.out.println("Ready for the tokens!");
 
--- src/test/java/com/hackerzhang/dust/ParserTest.java
+++ src/test/java/com/hackerzhang/dust/ParserTest.java
@@ -3,10 +3,26 @@ package com.hackerzhang.dust;
 import com.hackerzhang.dust.ast.ASTree;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ParserTest {
     @Test
     public void testParserRunner() throws ParseException {
         Lexer lexer = new Lexer(new CodeDialog());
+        BasicParser basicParser = new BasicParser();
+
+        while(lexer.peek(0) != Token.EOF) {
+            ASTree asTree = basicParser.parse(lexer);
+            System.out.println("=> " + asTree.toString());
+        }
+    }
+
+    @Test
+    public void testGistParser() throws ParseException, FileNotFoundException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        FileReader fileReader = new FileReader(classLoader.getResource("fixtures/gist2.txt").getFile());
+        Lexer lexer = new Lexer(fileReader);
         BasicParser basicParser = new BasicParser();
 
         while(lexer.peek(0) != Token.EOF) {
